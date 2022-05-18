026593ca6b5273b02893a91b88f60efced51b773
Luca Urbanowicz <human.exp11@gmail.com>
2022-05-18 10:56:53 -0600

    cleaning up folders and removing pages

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import '../utils/index.css';
+import React from "react";
+import "../utils/index.css";
 
 function Resume() {
-    return (
-<p>Please click <a href="./assets/LUR.pdf">here</a>to view my Resume.</p>
-    )
+  return (
+    <Wrapper>
+      <p>
+        Please click <a href="./assets/LUR.pdf">here</a>to view my Resume.
+      </p>
+    </Wrapper>
+  );
 }
-
 
 export default Resume;
